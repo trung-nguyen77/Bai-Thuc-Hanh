37f7dc656402f11c8c4ebf38a3e2743a38187440
trung-nguyen77 <nguyentrung0866707088@gmail.com>
2021-10-31 15:26:14 +0700

    complete round 1

---
+++ 18.Guardian of the Galaxy/Captain.js
@@ -0,0 +1,24 @@
+class Captain {
+    constructor(width, height) {
+        this.a = 150;
+        this.b = 465;
+        this.width = width;
+        this.height = height;
+        // this.color = "brown";
+    }
+    init(canvas) {
+        canvas.addEventListener('mousemove', function (event) {
+            processMouseMove(event);
+        })
+
+        function processMouseMove(event) {
+            let rect = canvas.getBoundingClientRect();
+            captain.a = event.clientX - 50;
+        }
+    }
+
+    render(canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.drawImage(captainImg, this.a, this.b, this.width, this.height)
+    }
+}
---
+++ 18.Guardian of the Galaxy/Meteors.js
@@ -0,0 +1,62 @@
+class Meteor {
+    constructor() {
+        this.x = 150;
+        this.y = 0;
+        this.speed = 1;
+    }
+
+    init() {
+        // let positions = [78, 178, 278, 437];
+        this.x = this.getRandomInt(); //lấy trong mảng position phần tử từ 0 đến 3
+
+    }
+
+    getRandomInt = function () {
+        return Math.floor(Math.random() * 400) ;//tạo hàm lấy số ngẫu nhiên
+    }
+
+
+    update(captain) {
+        if (this.y <= 450) {
+            this.y += (2 + this.speed);
+        }
+        this.checkHitCaptain(captain);
+    }
+
+    checkHitCaptain(captain) {
+
+        if (this.y + 70  >= 450) {
+            // console.log("----------------------------------------------")
+            // console.log(this.x)
+            // console.log(captain.a)
+            if ((this.x + 50 > captain.a) &&
+                (this.x + 50 < (captain.a + captain.width)) || (this.x  > captain.a) && (this.x  < (captain.a + captain.width)) ) {
+
+                this.y = 0;
+                score += 1;
+                this.speed += 2;
+                if (score > 10 && score % 5 === 0) {
+                    this.speed = 10
+                    this.render(canvas)
+                }
+                this.init();
+            } else {
+                this.y = 0;
+                alert('game over');
+                getName();
+                getPoint();
+                score = 0;
+                this.speed = 0;
+                drawGameOver();
+                clearSky();
+                // clearInterval(timerId);
+            }
+        }
+
+    }
+
+    render(canvas) {
+        let ctx = canvas.getContext('2d');
+        ctx.drawImage(meteorImg, this.x, this.y, 50, 85);
+    }
+}
